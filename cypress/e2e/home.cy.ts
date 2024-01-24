bed555e663d64badbf4b41b43399946b12b04ebf
Nnamdi Okeke <onnamdialex@gmail.com>
2024-01-24 18:49:31 +0100

    :white_check_mark: Update test / completion of a full block

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,8 @@
 describe("home spec", () => {
   it("the h1 contains the correct test", () => {
     cy.visit("http://localhost:3000")
-    cy.get("h1").should("be.visible")
+    cy.get("h1")
+      .should("be.visible")
+      .contains("Testing Next.js Applications with Cypress")
   })
 })
